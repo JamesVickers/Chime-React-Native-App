df091c641ead720b0e7328617399e9f397794f4a
Hannah Vickers <hvickersdesign@gmail.com>
2018-10-31 21:17:51 +0000

    react-native-sound set up

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import React, { Component } from 'react';
-import { TouchableHighlight, View, Text, StyleSheet, ImageBackground } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import {
+  TouchableHighlight,
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground
+} from "react-native";
+import { createStackNavigator } from "react-navigation";
+
+//must type react-native link react-native-sound in command line
+var Sound = require("react-native-sound");
+Sound.setCategory("Playback");
+var bell = new Sound("bell.wav", Sound.MAIN_BUNDLE, error => {
+  if (error) {
+    console.log("failed to load the sound", error);
+  } else {
+    console.log("duration in seconds: " + bell.getDuration());
+  }
+});
+
+function playBell() {
+  bell.play(success => {
+    if (success) {
+      console.log("successfully finished playing");
+    } else {
+      console.log("playback failed due to audio decoding errors");
+    }
+  });
+}
 
 class HomeScreen extends Component {
   render() {
     return (
-      <ImageBackground 
-        source={require('./assets/img/smoke.png')}
-        style={mainStyles.mainView}>
+      <ImageBackground
+        source={require("./assets/img/smoke.png")}
+        style={mainStyles.mainView}
+      >
         <Text>Chime</Text>
-        <TouchableHighlight 
+        <TouchableHighlight
           title="Enter"
-          onPress={() => this.props.navigation.navigate('Details')}>
+          onPress={() => this.props.navigation.navigate("Details")}
+        >
           <View>
             <Text>Enter</Text>
           </View>
@@ -24,14 +53,16 @@ class HomeScreen extends Component {
 class welcomeScreen extends Component {
   render() {
     return (
-      <ImageBackground 
-        source={require('./assets/img/smoke.png')}
-        style={mainStyles.mainView}>
+      <ImageBackground
+        source={require("./assets/img/smoke.png")}
+        style={mainStyles.mainView}
+      >
         <View style={mainStyles.modalView}>
           <Text>Welcome</Text>
           <TouchableHighlight
             title="Click here to begin..."
-            onPress={() => this.props.navigation.navigate('Creators')}>
+            onPress={() => this.props.navigation.navigate("TimerScreen")}
+          >
             <View>
               <Text>Click here to begin...</Text>
             </View>
@@ -45,40 +76,46 @@ class welcomeScreen extends Component {
 class TimerScreen extends Component {
   render() {
     return (
-      <ImageBackground 
-        source={require('./assets/img/smoke.png')}
-        style={mainStyles.mainView}>
+      <ImageBackground
+        source={require("./assets/img/smoke.png")}
+        style={mainStyles.mainView}
+      >
         <Text>Timer Screen</Text>
+        <TouchableHighlight title="Play" onPress={() => playBell()}>
+          <View>
+            <Text>Play sound</Text>
+          </View>
+        </TouchableHighlight>
       </ImageBackground>
     );
   }
 }
 
-const mainStyles = StyleSheet.create ({
-  mainView : {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'space-around',
+const mainStyles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around"
   },
   modalView: {
-    alignItems: 'center', 
-    justifyContent: 'space-around',
-    width: '85%',
-    height: '85%',
-    backgroundColor: 'rgba(160, 160, 160, 0.85)',
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "85%",
+    height: "85%",
+    backgroundColor: "rgba(160, 160, 160, 0.85)",
     borderRadius: 5
   }
 });
 
 const RootStack = createStackNavigator(
   {
-  Home: HomeScreen,
-  Details: welcomeScreen,
-  Creators: TimerScreen,
-},
-{
-  initialRouteName: 'Home',
-}
+    Home: HomeScreen,
+    Details: welcomeScreen,
+    TimerScreen: TimerScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
 );
 
 export default class App extends Component {
